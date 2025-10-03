0e7ab7aefbed62920f1c4cb7bf7ccab94349592e
Rathin Bagchi <rathinb745@gmail.com>
2025-10-03 16:36:57 +0530

    make employee data Api

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -4,10 +4,14 @@ import { hashPassword } from "../../../../lib/hash";
 import { signJwt } from "../../../../lib/jwt";
 import { setTokenCookie } from "../../../../lib/cookies";
 import { signupSchema } from "../../../../lib/validations/Signup";
+import { Role } from "../../../../generated/prisma/client";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    if (body.role) {
+  body.role = Role[body.role.toUpperCase() as keyof typeof Role];
+}
 
     // ✅ validate using Zod
     const parsed = signupSchema.safeParse(body);
@@ -18,7 +22,7 @@ export async function POST(req: Request) {
       );
     }
 
-    const { name, email, password } = parsed.data;
+    const { name, email, password, role} = parsed.data;
 
     const existing = await prisma.user.findUnique({ where: { email } });
     if (existing) {
@@ -27,7 +31,7 @@ export async function POST(req: Request) {
 
     const hashedPassword = await hashPassword(password);
     const user = await prisma.user.create({
-      data: { name, email, password: hashedPassword },
+      data: { name, email, password: hashedPassword, role },
     });
 
     const token = signJwt({ userId: user.id, name: user.name, email: user.email, role: user.role });

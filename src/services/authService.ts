8248492b73294ba8d2992ad67807649e2f609967
DoniwaljiV2 <chetandoniwal404@gmail.com>
2025-10-13 10:31:25 +0530

    fix: resolve merge conflicts in ProfilePage and authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,26 +1,13 @@
 import api from "@/lib/api";
 
 export const loginUser = async (email: string, password: string) => {
-<<<<<<< HEAD
-      const res = await api.post("/auth/signin", { email, password });
-  // const res = await api.post("/auth/login", { email, password });
-=======
     //  const res = await api.post("/auth/signin", { email, password });
   const res = await api.post("/auth/login", { email, password });
->>>>>>> 7c642c2e296f5536f3a19465a91dd6b892ca6a92
     return res.data;
  };
  
  
-<<<<<<< HEAD
-  //  const res = await api.post("/auth/login", { email, password });
-  // return res.data;
-
-
- export const registerUser = async (
-=======
 export const registerUser = async (
->>>>>>> 7c642c2e296f5536f3a19465a91dd6b892ca6a92
   name: string,
   email: string,
   password: string,
